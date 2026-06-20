d401cefc498bd5c98813c6f8edc798d5c3e074bf
mlodywilkuuu <mlodywilkuu@gmail.com>
2026-06-20 17:40:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, BrowserView } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            webviewTag: true // Wymagane do tagu <webview>
+        }
+    });
+
+    // Ładowanie interfejsu przeglądarki
+    mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
